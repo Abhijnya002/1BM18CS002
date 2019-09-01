8d9effd5d9c9834abf5c52cb1b95e4b2fe2dd608
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-09-01 06:32:26 -0400

    Update FACTORIA.C

--- FACTORIA.C
+++ FACTORIA.C
@@ -1,15 +1,12 @@
 #include<stdio.h>
-#include<conio.h>
 int fact(int n);
 int main()
 {
 	int n,f;
-	clrscr();
 	printf("enter n=");
 	scanf("%d",&n);
 	f=fact(n);
 	printf("%d\n",f);
-	getch();
 	return 0;
 }
 int fact(int n)
