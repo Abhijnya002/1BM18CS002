e7cbcf6924403e604764449ce001351d16149e06
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2019-09-30 20:22:39 +0530

    Add files via upload

---
+++ count_node.c
@@ -0,0 +1,160 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+};
+typedef struct node *NODE;
+void display(NODE head);
+NODE insert_front(int item,NODE head);
+NODE getnode();
+NODE insert_pos(NODE head,int item,int pos);
+void insert_end(NODE head,int item);
+int count(NODE head);
+int main()
+{
+	NODE head=NULL;
+	int ch,ele,position,c;
+	printf("Enter 1 to inser at the front,3 to insert at a given position,2 to insert at the end,4 to display\n");
+	scanf("%d",&ch);
+	while(ch!=0)
+	{
+	switch(ch)
+	{
+		case 1:printf("enter the ele=\n");
+	 		scanf("%d",&ele);
+			head=insert_front(ele,head);
+			break;
+		case 2:printf("enter the ele=\n");
+	 		scanf("%d",&ele);
+			insert_end(head,ele);
+			break;
+		case 3:printf("enter the ele and position=\n");
+	 		scanf("%d%d",&ele,&position);
+			head=insert_pos(head,ele,position);
+			break;
+		case 4:display(head);
+		case 5:c=count(head);
+			printf("number of node in a list is %d\n",c);
+			break;
+		default:break;
+	}
+	printf("again Enter 1 to inser at the front,3 to insert at a given position,2 to insert at the end,4 to display=\n");
+	scanf("%d",&ch);
+	}
+}
+NODE insert_front(int item,NODE head)
+{
+	NODE p;
+	p=getnode();
+	p->data=item;
+	p->next=head;
+	head=p;
+	return head;
+}
+NODE getnode()
+{
+	NODE p;
+	p=(NODE)malloc(sizeof(struct node));
+	if(p!=NULL)
+	{
+	return p;
+	}
+	else
+	{
+	printf("memory could not be allocated");
+	exit(0);
+	}
+}
+void insert_end(NODE head,int item)
+{
+	NODE p,q;
+	q=getnode();
+	q->data=item;
+	p=head;
+	while(p->next!=NULL)
+	{
+		p=p->next;
+	}
+	p->next=q;
+	q->next=NULL;
+}
+NODE insert_pos(NODE head,int item,int pos)
+{
+	NODE curr,prev=NULL,newn;
+	int count=1;
+	newn=getnode();
+	newn->data=item;
+	newn->next=NULL;
+	if(head==NULL)
+	{
+		if(pos==1)
+		{
+			return newn;
+		}
+	
+		else
+		{
+			printf("invalid position\n");
+			return head;
+		}	
+	}
+	if(pos==1)
+	{
+	newn->next=head;
+	head=newn;
+	return head;
+	}
+	else
+	{
+		curr=head;
+		while(curr!=NULL&&count!=pos)
+		{
+			prev=curr;
+			curr=curr->next;
+			count++;
+		}
+		if(count==pos)
+		{
+			prev->next=newn;
+			newn->next=curr;
+			return head;
+		}
+		else
+		{
+			printf("invalid position");
+			return head;
+		}
+	}
+}
+void display(NODE head)
+{
+	NODE p;
+	if(head==NULL)
+	{
+		printf("list is empty");
+		exit(0);
+	}
+	p=head;
+	printf("contents of list=\n");
+	while(p!=NULL)
+	{
+		printf("%d\n",p->data);
+		p=p->next;
+	}
+} 
+int count(NODE head)
+{
+	int count=0;
+	NODE p;
+	if(head==NULL)
+		return count;
+		p=head;
+		do
+		{
+		++count;
+		p=p->next;
+		}while(p!=NULL);
+		return count;
+}
